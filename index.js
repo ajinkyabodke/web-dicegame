fddb46fe691f8284e6c349844471da02cfc91163
Ajinkya Bodke <62661565+ajinkyabodke@users.noreply.github.com>
2020-07-28 10:38:13 +0530

    Added Web Files

---
+++ index.js
@@ -0,0 +1,16 @@
+var randomNumber1=Math.floor(Math.random()*6)+1;
+var randomNumber2=Math.floor(Math.random()*6)+1;
+var randomImageSource1="images/dice"+randomNumber1+".png";
+var randomImageSource2="images/dice"+randomNumber2+".png";
+
+document.querySelector(".img1").setAttribute("src",randomImageSource1);
+document.querySelector(".img2").setAttribute("src",randomImageSource2);
+
+if(randomNumber1>randomNumber2){
+  document.querySelector(".container h1").textContent="Player1 Wins!";
+}
+else if (randomNumber2>randomNumber1) {
+  document.querySelector(".container h1").textContent="Player2 Wins!";
+}else{
+  document.querySelector(".container h1").textContent="Draw!";
+}
